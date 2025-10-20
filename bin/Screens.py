8e491f1ca3a7b4ef1b0834bf92eb6cc3a8f83f35
Sasha Gontmakher <gsasha@gmail.com>
2025-10-20 22:17:07 +0300

    fix

--- bin/Screens.py
+++ bin/Screens.py
@@ -425,7 +425,7 @@ class NetworkScreen(BaseScreen):
         ipv4 = self.utils.get_ip()
         ping_status = self.utils.get_hassio_entity("binary_sensor.8_8_8_8", "state")
         ping_latency = self.utils.get_hassio_entity("sensor.8_8_8_8_round_trip_time_average", "state")
-        if ping_status == "ok":
+        if ping_status == "on":
           ping_line = f"8.8.8.8: {ping_latency}"
         else:
           ping_line = "8.8.8.8: disconnected"
